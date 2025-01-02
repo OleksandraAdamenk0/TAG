61d2a820215926b23ecc01c9ccc99103efd6887c
JagerDGrowly <vovkapedenko@gmail.com>
2025-01-02 17:14:01 +0100

    script

--- public/script.js
+++ public/script.js
@@ -7,12 +7,21 @@ const playArea = document.querySelector('.play-area');
 
 playArea.addEventListener('click', (e) => {
     const clickedCell = e.target;
-    if (this.contains(clickedCell) && clickedCell !== this) {
+    if (playArea.contains(clickedCell) && clickedCell !== playArea) {
         const emptyCell = document.getElementById('grid-empty');
 
         const clickedPosition = clickedCell.style.gridArea.split(' / ').map(Number);
         const emptyPosition = emptyCell.style.gridArea.split(' / ').map(Number);
 
+        console.log(clickedPosition, emptyPosition);
+
+        if ((Math.abs(clickedPosition[0] - emptyPosition[0]) === 1
+                && clickedPosition[1] - emptyPosition[1] === 0)
+            || (Math.abs(clickedPosition[1] - emptyPosition[1]) === 1
+                && clickedPosition[0] - emptyPosition[0] === 0)) {
+            emptyCell.style.gridArea = clickedPosition.join(' / ');
+            clickedCell.style.gridArea = emptyPosition.join(' / ');
+        }
         console.log(clickedPosition, emptyPosition);
     }
 })
