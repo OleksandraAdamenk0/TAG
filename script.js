874073e7515797c7a3e6b498a4043ac55af441db
JagerDGrowly <vovkapedenko@gmail.com>
2025-01-02 17:20:42 +0100

    fix

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ let cells = Array.from({ length: 16 }, (_, i) => i)
 
 console.log(cells);
 
+const playArea = document.querySelector('.play-area');
+
+playArea.addEventListener('click', (e) => {
+    const clickedCell = e.target;
+    if (playArea.contains(clickedCell) && clickedCell !== playArea) {
+        const emptyCell = document.getElementById('grid-empty');
+
+        const clickedPosition = clickedCell.style.gridArea.split(' / ').map(Number);
+        const emptyPosition = emptyCell.style.gridArea.split(' / ').map(Number);
+
+        console.log(clickedPosition, emptyPosition);
+
+        if ((Math.abs(clickedPosition[0] - emptyPosition[0]) === 1
+                && clickedPosition[1] - emptyPosition[1] === 0)
+            || (Math.abs(clickedPosition[1] - emptyPosition[1]) === 1
+                && clickedPosition[0] - emptyPosition[0] === 0)) {
+            emptyCell.style.gridArea = clickedPosition.join(' / ');
+            clickedCell.style.gridArea = emptyPosition.join(' / ');
+        }
+        console.log(clickedPosition, emptyPosition);
+    }
+})
 
 function initTag() {
-    const playArea = document.querySelector('.play-area');
-
     for (let i = 0; i < 16; i++) {
         const newDiv = document.createElement('div');
-        newDiv.classList.add('common-grid', `grid${cells[i]}`);
+        newDiv.classList.add('common-grid');
 
         const row = Math.ceil((i + 1) / 4);
         const col = (i + 1) % 4 === 0 ? 4 : (i + 1) % 4;
